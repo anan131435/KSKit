17acee5659080580da2604daf7fd6e6b46fe4acf
hanzhifeng <hanzhifeng@sunlands.com>
2020-04-18 07:21:58 +0800

    初始化项目

---
+++ KSKit/KSKit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KSKit
+//
+//  Created by 韩志峰 on 2020/4/18.
+//  Copyright © 2020 www.KS.net. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KSKit/KSKit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KSKit
+//
+//  Created by 韩志峰 on 2020/4/18.
+//  Copyright © 2020 www.KS.net. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
